e989f0efe1904efefe7708a9e8c930cbb0b45316
Sihayah <sihaya_a@hotmail.com>
2022-02-17 16:28:29 -0500

    added form handlers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -25,6 +26,7 @@ function App() {
        currentCategory={currentCategory}>
      </Nav>
      <main>
+       <ContactForm></ContactForm>
        <Gallery currentCategory={currentCategory}></Gallery>
        <About />
      </main>
